0003d9f62c66e2f2aa90baa22f9f7a8d95f29b16
Nick-Cho <nicholas.cho@hotmail.ca>
2023-01-01 09:33:22 -0500

    Saving Progress on Navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import { store } from './redux/store';
 
-export const RootState = ReturnType<typeof store.getState>
-
-export type AppDispatch = typeof store.dispatch
-
 function App() {
   return (
     <div>
