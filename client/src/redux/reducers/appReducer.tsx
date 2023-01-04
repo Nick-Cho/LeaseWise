bad0c49e07508f9881519a72d41b9328c4c3c5e4
Nick-Cho <nicholas.cho@hotmail.ca>
2023-01-04 13:20:10 -0500

    Adding userLoggedIn store variable

--- client/src/redux/reducers/appReducer.tsx
+++ client/src/redux/reducers/appReducer.tsx
@@ -3,8 +3,10 @@ import {RootState} from "../store";
 
 export const initialAppState = {
     navBarVisible: true,
+    userLoggedIn: false,
 }
 
+const setUserLoggedIn = createAction<boolean>("app/setUserLoggedIn");
 const setNavBarVisible = createAction<boolean>("app/setNavBarVisible");
 
 export const appReducer = createReducer(initialAppState, (builder)=>{
@@ -12,8 +14,15 @@ export const appReducer = createReducer(initialAppState, (builder)=>{
         .addCase(setNavBarVisible, (state, action) => {
             state.navBarVisible = action.payload;
         })
+        .addCase(setUserLoggedIn, (state,action) => {
+            state.userLoggedIn = action.payload;
+        })
 })
 
 export const getNavBarVisible = (state: RootState) => {
-    return state.appReducer.navBarVisible
+    return state.appReducer.navBarVisible;
+}
+
+export const getUserLoggedIn = (state: RootState) => {
+    return state.appReducer.userLoggedIn;
 }
